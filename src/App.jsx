7156da0235b3b1fdd4ac211ab2e33164aa1d1abe
Cassandra Keddis <cassandra@thinkific.com>
2023-07-20 14:38:54 -0700

    reference objects in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 
-const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
     </div>
   );
 }
