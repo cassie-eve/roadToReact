a4a4a680dc4bc3b364f33568638ff8b5d173a237
Cassandra Keddis <cassandra@thinkific.com>
2023-07-25 11:45:44 -0700

    feat(eslint): add linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-}
+const title = 'React';
 
 function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>Hello {title}</h1>
 
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
