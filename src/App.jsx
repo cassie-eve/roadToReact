a5022fdcfbb27bfd6ea9cf0a6561542fe62a993b
Cassandra Keddis <cassandra@thinkific.com>
2023-08-03 22:14:21 -0700

    feat(app): including handlers

--- src/App.jsx
+++ src/App.jsx
@@ -53,10 +53,14 @@ const List = () => {
 }
 
 const Search = () => {
+  const handleChange = (event) => {
+    console.log(event)
+    console.log(event.target.value)
+  };
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <input type="text" id="search" onChange={handleChange}/>
     </div>
   );
 }
